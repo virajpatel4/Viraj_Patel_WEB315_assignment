cfe037253994a6804b5223630f4063651e6c5416
virajpatel4 <VP97@myscc.ca>
2022-03-28 16:07:42 -0400

    save changes

---
+++ assignment2/VirajPatelGroceryApp/FoodItem.cs
@@ -0,0 +1,23 @@
+public class FoodItem
+{
+    public int Quantity { get; set; }
+
+    public int ProductId { get; set;}
+
+    public string Product { get; set; }
+
+    public int Price {get; set;}
+
+     public void IncreaseQuaniity()
+    {
+        if(Quantity<20)
+            Quantity++;
+    }
+
+    public void DescreaseQuaniity()
+    {
+        if(Quantity>0)
+            Quantity--;
+    }
+
+}
---
+++ assignment2/VirajPatelGroceryApp/GroceryIsle.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+public class GroceryIsle
+{
+
+    public List<FoodItem> FoodItemsList{ get; set; }
+    public string isleName  { get; set; }
+    public int isleNumber  { get; set; }
+
+    public GroceryIsle()
+    {
+        FoodItemsList=new List<FoodItem>();
+    }
+
+    public void AddFoodItem(FoodItem foodItem)
+    {
+        FoodItemsList.Add(foodItem);
+    }
+
+}
